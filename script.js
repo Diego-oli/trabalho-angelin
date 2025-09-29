4c8786f16cf9f0129b8b2af7993ddda14340c65b
Diego-oli <diegodutradeoliveira0@gmail.com>
2025-09-29 09:05:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
-let sort = Math.floor(Math.random()*100)+1;
-console.log(sort);
+let sort = Math.floor(Math.random() * 100) + 1;
 let tentativas = 5;
-document.getElementById("chances").innerHTML= " tentativas "+ tentativas;
- let saida = document.getElementById(" output");
+document.getElementById("chances").innerHTML = "Tentativas: " + tentativas;
+let saida = document.getElementById("output");
 
-function clique(){
-    let valor = document.getElementById("input").value
+function clique() {
+    let valor = document.getElementById("input").value;
     let saida = document.getElementById("output");
 
-if(valor > 0 && valor <=100){
-    if( tentativas > 0){
-    if(valor == sort){
-        saida.innerHTML = "acertou!!!!";
-    } if(valor > sort){
-        saida.innerHTML = "o valor sorteado é menor";
-    } if(valor < sort){
-        saida.innerHTML = "o valor sorteado é maior ";
-    }
+    if (valor > 0 && valor <= 100) {
+        if (tentativas > 0) {
+            if (valor == sort) {
+                saida.innerHTML = "Acertou!!!!";
+                tentativas = 0; // Acertou, não há mais tentativas.
+                document.getElementById("btn").disabled = true;
+            } else if (valor > sort) {
+                saida.innerHTML = "O valor sorteado é menor";
+            } else if (valor < sort) {
+                saida.innerHTML = "O valor sorteado é maior";
+            }
 
-} else {
-    saida.innerHTML = "Está fora do intervalo";
-}
-{
-    tentativas--;
-    document.getElementById("chances").innerHTML = "tentativas" + tentativas;
-    if(tentativas == 0){
-        document.getElementById("btn").disabled = true;
-        if(valor == sort){
-            saida.innerHTML = "acertou";
+            tentativas--; // Decrementa as tentativas
+            document.getElementById("chances").innerHTML = "Tentativas: " + tentativas;
+
+            if (tentativas == 0) {
+                document.getElementById("btn").disabled = true;
+                if (valor != sort) {
+                    saida.innerHTML = "Acabaram as tentativas, o número sorteado era: " + sort;
+                }
+            }
         }
-        else{
-            saida.innerHTML = "Acabaram as tentativas, o numero sorteado era: " + sort;
-        }
+    } else {
+        saida.innerHTML = "Está fora do intervalo (1 a 100)";
     }
 }
-}
 
-tentativas--;
-document.getElementById("chances ").innerHTML= " tentativas "+ tentativas;
-if(tentativas ==0){
-    document.getElementById("btn").disabled = true;
-    saida.innerHTML = "Acabaram as tentativas o número sorteado era: " + sort;
-}
-
-}
-
-function novo(){
-    let sort = Math.floor(Math.random()*100)+1;
+function novo() {
+    sort = Math.floor(Math.random() * 100) + 1;
     tentativas = 5;
-    document.getElementById("chances ").innerHTML= "tentativas "+ tentativas;
+    document.getElementById("chances").innerHTML = "Tentativas: " + tentativas;
     document.getElementById("btn").disabled = false;
-    saida.innerHTML= ("&nbsp");
-    
-
+    saida.innerHTML = "&nbsp;"; // Limpa a saída
 }
